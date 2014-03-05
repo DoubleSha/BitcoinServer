ad9fa9a3b909eae279b0eca71663a582a8b36c01
Kevin Greene <kgreenek@gmail.com>
2014-03-04 19:19:36 -0800

    Intermediate commit.

--- src/main/java/BitcoinServer/Application.java
+++ src/main/java/BitcoinServer/Application.java
@@ -1,7 +1,5 @@
 package BitcoinServer;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
--- src/main/java/BitcoinServer/MainController.java
+++ src/main/java/BitcoinServer/MainController.java
@@ -1,20 +1,17 @@
 package BitcoinServer;
 
+import com.google.protobuf.ByteString;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
 public class MainController {
-    @RequestMapping(value = "/create_payment_request", method = RequestMethod.POST)
-    public ByteArray createPaymentRequest() throws IOException {
-        return "fail";
-    }
+    @RequestMapping(value = "/broadcast_transactions", method = RequestMethod.POST)
+    public ByteString broadcastTransactions() throws IOException {
 
-    @RequestMapping(value = "/broadcast_tx", method = RequestMethod.POST)
-    public ByteArray broadcastTx() throws IOException {
-        return "fail";
+        return ByteString.copyFromUtf8("fail");
     }
 }
