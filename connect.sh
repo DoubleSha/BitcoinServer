fe9783acb3fd219c5658a243c6946caa150b9874
Kevin Greene <kgreenek@gmail.com>
2014-04-22 02:47:29 -0700

    Updating upload and connect script

--- connect.sh
+++ connect.sh
@@ -1,1 +1,1 @@
-ssh -i ~/misito-ec2-0.pem ubuntu@54.186.90.163
+ssh -i ~/misito-ec2-0.pem ubuntu@54.187.102.177
--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
-scp -i ~/misito-ec2-0.pem -r build/libs/BitcoinServer-0.1.0.jar ubuntu@54.186.90.163:~/
-scp -i ~/misito-ec2-0.pem -r start.sh ubuntu@54.186.90.163:~/
+scp -i ~/misito-ec2-0.pem -r build/libs/BitcoinServer-0.1.0.jar ubuntu@54.187.102.177:~/
+scp -i ~/misito-ec2-0.pem -r start.sh ubuntu@54.187.102.177:~/
